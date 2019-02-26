3f4a99162dd800352bfb0e067a78900516a6126a
warfee <warfee619916@gmail.com>
2019-02-26 13:09:17 +0000

    Update App.js
    warfee as stud2

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Iffat 1611279</Text>
+        <Text>Iffat 1611279,im adding some text in this file, warfee 1616757</Text>
       </View>
     );
   }
